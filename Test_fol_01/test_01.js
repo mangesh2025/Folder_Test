63d6350368eb15f0138a511b9bb5010b0c1c9a85
Mangesh pal <mangeshpal2025@gmail.com>
2024-08-24 11:19:43 +0530

    folder test

---
+++ Test_fol_01/test_01.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const test_01 = () => {
+  return (
+    <div>test_01</div>
+  )
+}
+
+export default test_01
---
+++ Test_fol_02/test_02.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const test_02 = () => {
+  return (
+    <div>test_02</div>
+  )
+}
+
+export default test_02
